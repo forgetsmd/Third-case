1653e3d332869dcaaacbabfb83aff87db7b33c1b
forgetsmd <forgetsmd@gmail.com>
2020-10-29 01:04:17 +0300

    modified_for

--- test.c
+++ test.c
@@ -4,14 +4,29 @@ int main (void)
 {
   int arr[10];
   for (int i = 0; i <= 9; i++)
+  {
+    if (i == 4)
     {
-    arr[i] = 1;
+      arr[i] = 3;
     }
+    else if (i == 5)
+    {
+      arr[i] = 2;
+    }
+    else if (i % 2 == 0)
+    {
+      arr[i] = 0;
+    }
+    else
+    {
+      arr[i] = 1;
+    }
+  }
   int cnt = 0;
   while (cnt < 10)
-    {
+  {
     printf ("%d", arr[cnt]);
     cnt++;
-    }
+  }
   return 0;
 }
